14219da10e8c4678c99e17bb3fecc5539d179324
OpeOginni <brightoginni123@gmail.com>
2023-09-23 20:10:23 +0100

    Display User Wallet Address in HomePage

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -20,6 +20,7 @@ export default async function Dashboard() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Dashboard
       <div>Welcome {user.name}</div>
+      <div>Wallet: {user.walletAddress}</div>
       <SignOutButton />
     </main>
   );
