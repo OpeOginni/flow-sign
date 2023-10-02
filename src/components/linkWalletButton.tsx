7f995a32ad8e7a9f55467ae0cd60c93976273837
OpeOginni <brightoginni123@gmail.com>
2023-10-02 10:24:26 +0100

    Started Hybrid Custody Implementation

--- src/components/linkWalletButton.tsx
+++ src/components/linkWalletButton.tsx
@@ -1,15 +1,102 @@
 "use client";
-import { signOut } from "next-auth/react";
+import * as fcl from "@onflow/fcl";
+import React, { useState, useEffect } from "react";
 
-export default function LinkWalletButton() {
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  claimChildAccount,
+  publishChildAccount,
+  setupChildAccount,
+} from "@/utils/hybrid-custody";
+
+interface LinkWalletButtonProps {
+  flowSignWalletAddress: string;
+  flowSignWalletPrivateKey: string;
+}
+
+interface FclUserType {
+  addr: string;
+  loggedIn: boolean;
+}
+
+export default function LinkWalletButton(props: LinkWalletButtonProps) {
+  const [user, setUser] = useState({ loggedIn: null, addr: "" });
+
+  useEffect(() => {
+    const unsubscribe = fcl.currentUser.subscribe((user: any) => setUser(user));
+    return () => unsubscribe();
+  }, []); // sets the callback for FCL to use
+
+  async function walletLinking() {
+    try {
+      console.log(props);
+
+      console.log(user);
+
+      // await setupChildAccount(
+      //   props.flowSignWalletPrivateKey,
+      //   props.flowSignWalletAddress
+      // );
+
+      console.log("Child Account Setup");
+
+      await publishChildAccount(
+        user.addr,
+        props.flowSignWalletPrivateKey,
+        props.flowSignWalletAddress
+      );
+      console.log("Child Published");
+
+      await claimChildAccount(await fcl.authz, props.flowSignWalletAddress);
+
+      console.log("Child Claimed");
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
-    <div>
-      <button
-        onClick={() => signOut({ callbackUrl: "/" })}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Link Wallet
-      </button>
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Link Wallet
+        </button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogDescription>
+            {user.loggedIn ? (
+              <>
+                <button
+                  onClick={async () => await walletLinking()}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Link Wallet
+                </button>
+                <button
+                  onClick={() => fcl.unauthenticate()}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Log Out Wallet
+                </button>
+              </>
+            ) : (
+              <button
+                onClick={() => fcl.logIn()}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Login Wallet
+              </button>
+            )}
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
