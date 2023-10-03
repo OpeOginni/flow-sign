a93b84bd76f27b88addf02138ee68dc594aa0c52
OpeOginni <brightoginni123@gmail.com>
2023-10-03 21:08:11 +0100

    Removed GitHub Provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -44,10 +44,6 @@ export const authOptions: NextAuthOptions = {
         }
     },
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!
-        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
